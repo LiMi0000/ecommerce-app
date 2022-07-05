1c6df12e6f50ca421e8938aa13bb8b764921b217
LiMi-SEDC <limimisini@hotmail.com>
2022-07-05 12:38:53 +0200

    fetch some data

---
+++ components/index.js
@@ -0,0 +1,17 @@
+// import HeroBanner from './HeroBanner';
+// import Cart from './Cart';
+// import Footer from './Footer';
+// import FooterBanner from './FooterBanner';
+// import Layout from './Layout';
+// import Navbar from './Navbar';
+// import Product from './Product';
+
+// export { HeroBanner, Cart, Footer, FooterBanner, Layout, Navbar, Product };
+
+export { default as HeroBanner } from './HeroBanner';
+export { default as Cart } from './Cart';
+export { default as Footer } from './Footer';
+export { default as FooterBanner } from './FooterBanner';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as Product } from './Product';
