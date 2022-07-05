099299fe44b4996dfbd2d154df05fa7a351202fc
LiMi-SEDC <limimisini@hotmail.com>
2022-07-05 19:44:28 +0200

    products

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { HeroBanner, FooterBanner, Product } from '../components';
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       {console.log(bannerData)}
       <div className="products-heading">
@@ -14,7 +14,9 @@ const Home = ({ products, bannerData }) => {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
