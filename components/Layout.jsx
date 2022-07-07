cbdcfbad211cd2fa55ba3f041fffa7ac384c9ba5
LiMi-SEDC <limimisini@hotmail.com>
2022-07-07 19:08:28 +0200

    working on navbar & footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
-  return <div>Layout</div>;
+import { Navbar, Footer } from '../components';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Headphones Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
